58ee766f1e94ec34835c9a96ef19aa4872e8a87e
Takuya Jumbo Nakamura <takedhy@gmail.com>
2021-08-07 15:22:21 +0900

    出力の値が間違っていたので修正

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 input.onButtonPressed(Button.A, function () {
-    pins.digitalWritePin(DigitalPin.P0, 0)
+    pins.digitalWritePin(DigitalPin.P0, 1)
 })
 input.onButtonPressed(Button.AB, function () {
     pins.analogWritePin(AnalogPin.P0, 100)
